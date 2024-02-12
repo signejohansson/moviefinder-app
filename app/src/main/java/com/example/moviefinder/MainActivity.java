fcbb60f5d0cd8270213b9cae5c0ef027e46537fa
Signe Johansson <signe.johansson.5006@student.lu.se>
2024-02-12 15:40:18 +0100

    added a back button

--- app/src/main/java/com/example/moviefinder/MainActivity.java
+++ app/src/main/java/com/example/moviefinder/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.moviefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText movieNameEditText;
+    private Button searchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        movieNameEditText = findViewById(R.id.movie);
+        searchButton = findViewById(R.id.search_btn);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String movieName = movieNameEditText.getText().toString().trim();
+                if (!movieName.isEmpty()) {
+                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                    intent.putExtra("movie", movieName);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter a movie title", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
