415bfe2120a9e5c6788f3547619bb555211631d2
Signe Johansson <signe.johansson.5006@student.lu.se>
2024-02-14 14:12:19 +0100

    changed to lambda expression

--- app/src/main/java/com/example/moviefinder/MainActivity.java
+++ app/src/main/java/com/example/moviefinder/MainActivity.java
@@ -11,24 +11,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText movieNameEditText;
-    private Button searchButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         movieNameEditText = findViewById(R.id.movie);
-        searchButton = findViewById(R.id.search_btn);
-        searchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String movieName = movieNameEditText.getText().toString().trim();
-                if (!movieName.isEmpty()) {
-                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-                    intent.putExtra("movie", movieName);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(MainActivity.this, "Please enter a movie title", Toast.LENGTH_SHORT).show();
-                }
+        Button searchButton = findViewById(R.id.search_btn);
+        searchButton.setOnClickListener(v -> {
+            String movieName = movieNameEditText.getText().toString().trim();
+            if (!movieName.isEmpty()) {
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putExtra("movie", movieName);
+                startActivity(intent);
+            } else {
+                Toast.makeText(MainActivity.this, "Please enter a movie title", Toast.LENGTH_SHORT).show();
             }
         });
     }
